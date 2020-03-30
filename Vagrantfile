7291e37b2e972f10849c4d816a24e29479c73e7d
Robert Ashton <r.ashton@imperial.ac.uk>
2020-03-30 11:03:09 +0100

    Add script to login to docker and start agent

--- Vagrantfile
+++ Vagrantfile
@@ -1,5 +1,23 @@
+vault_config_file = 'provision/vault_config'
+
+if !File.file?(vault_config_file)
+  raise "Run ./provision/vault-prepare.sh first!"
+end
+
+vault_config = Hash[File.read(vault_config_file).split("\n").
+                     map{|s| s.split('=')}]
+
 Vagrant.configure("2") do |config|
   config.vm.box = "builds/virtualbox-ubuntu1804.box"
   config.ssh.username = "vagrant"
   config.ssh.password = "vagrant"
+
+  config.vm.provision :shell do |shell|
+    shell.path = 'provision/start_agent.sh'
+    shell.env = vault_config
+    shell.privileged = false
+  end
+    
 end
+
+
---
+++ provision/start_agent.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+if [ `id -u` -eq 0 ]; then
+    echo "This script is designed to be run by an ordinary user"
+    exit 1
+fi
+
+## Login to vault with token
+vault login -method=github
+
+## Read docker info from vault
+DOCKER_USERNAME=`vault read -field=user secret/registry`
+vault read -field=password secret/registry | \
+  docker login -u $DOCKER_USERNAME --password-stdin
+
+
+## Read buildkite agent token from vault and write into cfg
+BUILDKITE_AGENT_TOKEN=`vault read -field=token secret/buildkite/agent`
+sed -i "s/xxx/${BUILDKITE_AGENT_TOKEN}/g" /etc/buildkite-agent/buildkite-agent.cfg
+
+## Startup agent
+sudo systemctl enable buildkite-agent && sudo systemctl start buildkite-agent

3906623bf1a67846c3ad1486765fbefe939cfa81
Wes Hinsley <w.hinsley@imperial.ac.uk>
2020-10-07 15:41:33 +0100

    Detect vmhost for agent metadata

--- provision/vault-prepare.sh
+++ provision/vault-prepare.sh
@@ -3,6 +3,7 @@ set -e
 HERE=$(dirname $(dirname $(realpath $0)))
 VAULT_CONFIG=$HERE/vault_config
 
+export VMHOST_NAME=$(hostname)
 export VAULT_ADDR='https://vault.dide.ic.ac.uk:8200'
 if [ "$VAULT_AUTH_GITHUB_TOKEN" = "" ]; then
     echo -n "Please provide your GitHub personal access token for the vault: "
@@ -12,4 +13,4 @@ if [ "$VAULT_AUTH_GITHUB_TOKEN" = "" ]; then
 fi
 
 echo "Writing vault token to $VAULT_CONFIG"
-env | grep -E '^(VAULT_ADDR|VAULT_AUTH_GITHUB_TOKEN)' > $VAULT_CONFIG
+env | grep -E '^(VAULT_ADDR|VAULT_AUTH_GITHUB_TOKEN|VMHOST_NAME)' > $VAULT_CONFIG
